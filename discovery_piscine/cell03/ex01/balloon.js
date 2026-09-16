3c4416f9f7e98f9ee920773e8822b3c36d17f7b1
izcazy <ice.izcazy@gmail.com>
2026-09-16 17:57:44 +0700

    update

---
+++ discovery_piscine/cell03/ex01/balloon.js
@@ -0,0 +1,30 @@
+let size = 200;
+const colors = ['red', 'green', 'blue'];
+let colorIndex = 0;
+
+const balloon = document.getElementById('balloon');
+
+balloon.addEventListener('click', () => {
+    size += 10;
+    if (size > 420) {
+        size = 200;
+        colorIndex = 0;
+    } else {
+        colorIndex = (colorIndex + 1) % colors.length;
+    }
+    updateBalloon();
+});
+
+balloon.addEventListener('mouseleave', () => {
+    if (size > 200) {
+        size -= 5;
+        colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+    }
+    updateBalloon();
+});
+
+function updateBalloon() {
+    balloon.style.width = size + 'px';
+    balloon.style.height = size + 'px';
+    balloon.style.backgroundColor = colors[colorIndex];
+}
