9c1d5486ab808861fceb04acfc76699478565d23
izcazy <ice.izcazy@gmail.com>
2026-09-16 15:55:23 +0700

    updste

---
+++ discovery_piscine/cell03/ex00/background.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    document.body.style.backgroundColor = randomColor;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('bgBtn');
+    button.addEventListener('click', changeBackgroundColor);
+});
